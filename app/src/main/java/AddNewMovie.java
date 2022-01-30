60a9a0d900a7852cd28fa1afc957653a024c7706
Josh Lau <josh.lau@coinbase.com>
2022-01-30 15:55:41 -0500

    Add new movie task creation dialog

---
+++ app/src/main/java/AddNewMovie.java
@@ -0,0 +1,10 @@
+import android.widget.EditText;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class AddNewMovie extends BottomSheetDialogFragment {
+    public static final String TAG = "ActionBottomDialog";
+
+    private EditText newMovieText;
+    
+}
